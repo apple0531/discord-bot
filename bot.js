52852894c161b474352d05747ac564f24036b8b7
tubbyyyy <xeroxmercy01@gmail.com>
2018-11-05 12:28:20 -0500

    fixed message handlers

--- bot.js
+++ bot.js
@@ -9,22 +9,17 @@ client.on("ready", () => {
   client.user.setStatus("available");
   client.user.setActivity("tetris poorly"); });
 
-const commands = { //list of message for be sent to the user
-  "ping": "Pong!",
-  "hello": "world",
-  "github": "github.com/tubbyyyy"}
-
 client.on("message", (message) => {
   if(message.author.bot){return;} //if its a bot - disregard
   if(message.content[0] !== configuration.prefix){return;} //if its not a command by the user - disregard
 
   message.content = message.content.substring(1).toLocaleLowerCase();
     //used to deal with messages
-    if(commands[message.content]){message.reply(commands[message.content]);}
+    if(commands[message.content]){message.reply(configuration.commands[message.content]);}
     //used to deal with commands
     if(message.content === "name"){message.reply("Your username is " + message.author.username);}
 
     if(message.content === "help"){
       let reply = "The commands are \n";
-      for(command in commands){reply += command + "\n";}
+      for(command in configuration.commands){reply += command + "\n";}
       message.reply(reply);} });
