343870761e77db14f9915deb10912fa096c87ced
tubbyyyy <xeroxmercy01@gmail.com>
2018-11-06 13:33:27 -0500

    fixed command

--- bot.js
+++ bot.js
@@ -14,12 +14,12 @@ client.on("message", (message) => {
   if(message.content[0] !== configuration.prefix){return;} //if its not a command by the user - disregard
 
   message.content = message.content.substring(1).toLocaleLowerCase();
-    //used to deal with messages
+    //used to deal with message requests
     if(configuration.commands[message.content]){message.reply(configuration.commands[message.content]);}
-    //used to deal with commands
-    if(message.content === "name"){message.reply("Your username is " + message.author.username);}
 
+    //used to deal with commands for information
+    if(message.content === "name"){message.reply("Your username is " + message.author.username);}
     if(message.content === "help"){
-      let reply = "The commands are \n";
+      let reply = "\nThe commands are: \n";
       for(command in configuration.commands){reply += command + "\n";}
       message.reply(reply);} });
